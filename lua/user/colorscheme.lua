e62fe0aebf4b84e69e9ec9709204690b08978b61
Neal Ó Riain <neal.o.riain@gmail.com>
2022-10-20 19:51:39 +0100

    Add files via upload

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,15 @@
+local colorscheme = "onedark"
+
+require('onedark').setup  {
+    -- Main options --
+    style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+    transparent = true,  -- Show/hide background
+    term_colors = true, -- Change terminal color as per the selected theme style
+    ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+    cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+}
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  return
+end
---
+++ lua/user/keymaps.lua
@@ -0,0 +1,42 @@
+-- Shorten function name
+local keymap = vim.keymap.set
+-- Silent keymap option
+local opts = { silent = true }
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Clear highlights
+keymap("n", "\\", "<cmd>nohlsearch<CR>", opts)
+
+-- Better paste
+keymap("v", "p", '"_dP', opts)
+
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+
+-- NvimTree
+keymap("n", "<C-n>", ":NvimTreeToggle<CR>", opts)
+
+keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
+
+-- Comment
+keymap("n", "cc", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", opts)
+
+-- delete in normal mode
+keymap("n", "<bs>", "X", opts)
+
+-- remap pane keys
+keymap("n", "<Tab>", "<C-w>w", opts)
+keymap("n", "<Bar>", ":vsplit<CR>", opts)
+
+-- fix the Q/q
+vim.cmd("command Q q")
+vim.cmd("command W w")
